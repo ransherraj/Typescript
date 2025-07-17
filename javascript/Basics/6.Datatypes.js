b31b4ab6b3c8962f3864458cdbdca3947c3a9651
RANSHER RAJ VERMA <44178727+ransherraj@users.noreply.github.com>
2025-07-18 01:54:50 +0530

    Create 6.Datatypes.js

---
+++ javascript/Basics/6.Datatypes.js
@@ -0,0 +1,28 @@
+
+// What are JavaScript data types?
+
+🔸 Primitive Types (immutable)
+          string
+          
+          number
+          
+          bigint
+          
+          boolean
+          
+          undefined
+          
+          null
+          
+          symbol
+
+🔸 Non-Primitive (reference) Types
+          object (includes arrays, functions)
+
+🔸 Example:
+let str = "Hello";        // string
+let num = 42;             // number
+let isReady = false;      // boolean
+let val = undefined;      // undefined
+let empty = null;         // null
+let obj = { key: "value" }; // object
